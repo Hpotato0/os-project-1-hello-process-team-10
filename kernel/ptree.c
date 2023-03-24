7cd0dd833a47964faa96f7e2d0d654f4668540bd
Hpotato0 <hpotato@snu.ac.kr>
2023-03-24 22:20:48 +0900

    starting to convert pseudocode to code

--- kernel/ptree.c
+++ kernel/ptree.c
@@ -5,6 +5,14 @@
 #include <linux/gfp.h> // GFP_KERNEL
 #include <uapi/asm-generic/errno-base.h> // error codes
 
+static void savePinfoToBuff(struct task_struct* taskptr, struct pinfo* pBuff, unsigned int cur_depth){
+    pBuff->state = taskptr->state;
+    pBuff->pid = taskptr->pid;
+    pBuff->uid = (taskptr->cred)->uid;
+    get_task_comm(pBuff->comm, taskptr);
+    pBuff->depth = cur_depth;
+}
+
 SYSCALL_DEFINE2(ptree, struct pinfo *, buf, size_t, len)
 {
     size_t alloc_unit;  // allocation unit
@@ -14,8 +22,9 @@ SYSCALL_DEFINE2(ptree, struct pinfo *, buf, size_t, len)
 
     // for traversing tree
     struct task_struct* taskptr;
-    int going_down;
-    int cur_depth;
+    int going_down = 1;
+    int from_child = 0;
+    unsigned int cur_depth = 0;
 
     // debug msg
     printk("running syscall ptree\n");
@@ -33,18 +42,73 @@ SYSCALL_DEFINE2(ptree, struct pinfo *, buf, size_t, len)
     taskptr = &init_task;
     pcount = 0;
     going_down = 1;
+    
     while(pcount < len){
+        size_t start;
         size_t end;
-        if(pcount + alloc_unit > len) end = len - pcount;
-        else end = alloc_unit;
-        pcount += alloc_unit;
+
+        start = pcount;
+        if(pcount + alloc_unit > len) end = len;
+        else end = pcount + alloc_unit;
 
         read_lock(&tasklist_lock);
+        while(pcount < end){
+            //control: going_down, from_child
+            //counter: depth, copied_num
+            //depth is changed when jump to next node
+            //copied_num is changed when taskptr is visited
+            //goint_down & from_child is changed when jump to next node
+            size_t idx = 0;
+            if(cur_depth == 0 && going_down==0) break;
 
-        for(size_t idx = 0; idx < end; idx++){
-            SAVE taskptr to pBuff[idx]
-            IF taskptr has child:
-                going_down
+            if(going_down){ // must visit taskptr
+                savePinfoToBuff(taskptr, pBuff+idx, cur_depth);                
+                idx++; pcount++;
+
+                if(!list_empty(taskptr->children)){
+                    taskptr = list
+                }
+                else{
+
+                }
+
+                IF taskptr has child: // go to child
+                    taskptr = leftmost child of taskptr;
+                    cur_depth++
+                ELSE: // check if taskptr has sibling. If so, go to sibling, if not, go to parent
+                    going_down = 0;
+                    IF taskptr has next sibling:
+                        taskptr = next sibling of taskptr;
+                    ELSE:
+                        taskptr = parent of taskptr;
+                        cur_depth--;
+                        from_child = 1;
+            }
+            else{
+                IF from_child is 1: // last node was its child
+                    IF taskptr has next sibling:
+                        from_child = 0;
+                        taskptr = next sibling of taskptr;
+                    ELSE: // taskptr doesn't have next sibling => go to parent
+                        taskptr = parent of taskptr;
+                        from_child = 1;
+                        cur_depth--;
+                ELSE: // last node was its sibling, must visit taskptr
+                    copied_num++;
+                    SAVE taskptr and cur_depth to pBuff[idx];
+
+                    IF tasktpr has child:
+                        going_down = 1;
+                        taskptr = child of taskptr;
+                        cur_cepth++;
+                    ELSE: // taskptr doesn't have child
+                        IF takptr has next sibling:
+                            tasktpr = next sibling of taskptr
+                        ELSE: // taskptr doesn't have next sibling
+                            from_child = 1;
+                            cur_depth--;
+                            taskptr = parent of taskptr;
+            }         
         }
 
         read_unlock(&tasklist_lock);
