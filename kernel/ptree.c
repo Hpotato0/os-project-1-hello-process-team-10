aa9eebbb05fa349acec898f7cf74449b6741b4ae
Hpotato0 <hpotato@snu.ac.kr>
2023-03-24 23:36:47 +0900

    it builds!

--- kernel/ptree.c
+++ kernel/ptree.c
@@ -4,12 +4,15 @@
 #include <linux/slab.h> // kzalloc
 #include <linux/gfp.h> // GFP_KERNEL
 #include <uapi/asm-generic/errno-base.h> // error codes
+#include <linux/cred.h> // cred->kuid_t
+#include <linux/sched.h> //TASK_COMM_LEN
 
 static void savePinfoToBuff(struct task_struct* taskptr, struct pinfo* pBuff, unsigned int cur_depth){
     pBuff->state = taskptr->state;
     pBuff->pid = taskptr->pid;
-    pBuff->uid = (taskptr->cred)->uid;
-    get_task_comm(pBuff->comm, taskptr);
+    pBuff->uid = (taskptr)->cred->uid.val;
+
+    __get_task_comm(pBuff->comm, TASK_COMM_LEN, taskptr);
     pBuff->depth = cur_depth;
 }
 
@@ -19,6 +22,7 @@ SYSCALL_DEFINE2(ptree, struct pinfo *, buf, size_t, len)
     size_t sz;          // size to allocate
     struct pinfo* pBuff;// allocated buffer pointer
     size_t pcount;      // pinfo read count
+    unsigned long failed_copy_bytes;// for checking copy_to_user failure
 
     // for traversing tree
     struct task_struct* taskptr;
@@ -46,11 +50,14 @@ SYSCALL_DEFINE2(ptree, struct pinfo *, buf, size_t, len)
     while(pcount < len){
         size_t start;
         size_t end;
+        size_t idx;
+        size_t sz_copy;
 
         start = pcount;
         if(pcount + alloc_unit > len) end = len;
         else end = pcount + alloc_unit;
 
+        idx = 0;
         read_lock(&tasklist_lock);
         while(pcount < end){
             //control: going_down, from_child
@@ -58,61 +65,67 @@ SYSCALL_DEFINE2(ptree, struct pinfo *, buf, size_t, len)
             //depth is changed when jump to next node
             //copied_num is changed when taskptr is visited
             //goint_down & from_child is changed when jump to next node
-            size_t idx = 0;
             if(cur_depth == 0 && going_down==0) break;
 
             if(going_down){ // must visit taskptr
                 savePinfoToBuff(taskptr, pBuff+idx, cur_depth);                
                 idx++; pcount++;
 
-                if(!list_empty(taskptr->children)){
-                    taskptr = list
+                if(!list_empty(&(taskptr->children))){
+                    taskptr = list_first_entry(&(taskptr->children), struct task_struct, children);
+                    cur_depth++;
                 }
                 else{
-
-                }
-
-                IF taskptr has child: // go to child
-                    taskptr = leftmost child of taskptr;
-                    cur_depth++
-                ELSE: // check if taskptr has sibling. If so, go to sibling, if not, go to parent
                     going_down = 0;
-                    IF taskptr has next sibling:
-                        taskptr = next sibling of taskptr;
-                    ELSE:
-                        taskptr = parent of taskptr;
-                        cur_depth--;
+                    if(!list_empty(&(taskptr->sibling))){
+                        taskptr = list_first_entry(&(taskptr->sibling), struct task_struct, sibling);
+                    }
+                    else{
+                        taskptr = taskptr->real_parent;
                         from_child = 1;
+                        cur_depth--;
+                    }
+                }
             }
             else{
-                IF from_child is 1: // last node was its child
-                    IF taskptr has next sibling:
+                if(from_child){ // last node was its child
+                    if(!list_empty(&(taskptr->sibling))){
                         from_child = 0;
-                        taskptr = next sibling of taskptr;
-                    ELSE: // taskptr doesn't have next sibling => go to parent
-                        taskptr = parent of taskptr;
+                        taskptr = list_first_entry(&(taskptr->sibling), struct task_struct, sibling);
+                    }
+                    else{ // taskptr doesn't have next sibling => go to parent
+                        taskptr = taskptr->real_parent;
                         from_child = 1;
                         cur_depth--;
-                ELSE: // last node was its sibling, must visit taskptr
-                    copied_num++;
-                    SAVE taskptr and cur_depth to pBuff[idx];
+                    }
+                }
+                else{// last node was its sibling, must visit taskptr
+                    savePinfoToBuff(taskptr, pBuff+idx, cur_depth);                
+                    idx++; pcount++;
 
-                    IF tasktpr has child:
+                    if(!list_empty(&(taskptr->children))){
                         going_down = 1;
-                        taskptr = child of taskptr;
-                        cur_cepth++;
-                    ELSE: // taskptr doesn't have child
-                        IF takptr has next sibling:
-                            tasktpr = next sibling of taskptr
-                        ELSE: // taskptr doesn't have next sibling
+                        taskptr = list_first_entry(&(taskptr->children), struct task_struct, children);
+                        cur_depth++;
+                    }
+                    else{
+                        if(!list_empty(&(taskptr->sibling))){
+                            taskptr = list_first_entry(&(taskptr->sibling), struct task_struct, sibling);
+                        }
+                        else{
+                            taskptr = taskptr->real_parent;
                             from_child = 1;
                             cur_depth--;
-                            taskptr = parent of taskptr;
-            }         
+                        }
+                    }
+                }
+            }
         }
-
         read_unlock(&tasklist_lock);
+
+        sz_copy = sizeof(struct pinfo) * idx;
+        failed_copy_bytes = copy_to_user(buf, pBuff, sz_copy);
     }
 
-    return 0;
+    return pcount;
 }
